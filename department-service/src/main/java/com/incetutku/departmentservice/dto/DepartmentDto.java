4cb108d355ab690ee44707b8efdbc3c5be2228de
Tutku Ince <tutku.ince@outlook.com>
2024-09-28 23:01:59 +0300

    Builder annotation has been added

---
+++ department-service/src/main/java/com/incetutku/departmentservice/dto/DepartmentDto.java
@@ -0,0 +1,15 @@
+package com.incetutku.departmentservice.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DepartmentDto {
+    private Long id;
+    private String name;
+    private String description;
+    private String code;
+}
--- department-service/src/main/java/com/incetutku/departmentservice/entity/Department.java
+++ department-service/src/main/java/com/incetutku/departmentservice/entity/Department.java
@@ -1,15 +1,13 @@
 package com.incetutku.departmentservice.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Table(name = "departments")
 @Entity
 public class Department {
