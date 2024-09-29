b0e091af1097ecc292fd48c1ed9305b76e75aa05
Tutku Ince <tutku.ince@outlook.com>
2024-09-29 19:40:18 +0300

    return type and implementation has been changed

--- employee-service/src/main/java/com/incetutku/employeeservice/service/EmployeeService.java
+++ employee-service/src/main/java/com/incetutku/employeeservice/service/EmployeeService.java
@@ -1,10 +1,11 @@
 package com.incetutku.employeeservice.service;
 
+import com.incetutku.employeeservice.dto.APIResponseDto;
 import com.incetutku.employeeservice.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto save(EmployeeDto employeeDto);
 
-    EmployeeDto getById(Long id);
+    APIResponseDto getById(Long id);
 }
 
--- employee-service/src/main/java/com/incetutku/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/incetutku/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,20 +1,26 @@
 package com.incetutku.employeeservice.service.impl;
 
+import com.incetutku.employeeservice.dto.APIResponseDto;
+import com.incetutku.employeeservice.dto.DepartmentDto;
 import com.incetutku.employeeservice.dto.EmployeeDto;
 import com.incetutku.employeeservice.entity.Employee;
 import com.incetutku.employeeservice.mapper.EmployeeMapper;
 import com.incetutku.employeeservice.repository.EmployeeRepository;
 import com.incetutku.employeeservice.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private final RestTemplate restTemplate;
 
-    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, RestTemplate restTemplate) {
         this.employeeRepository = employeeRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Override
@@ -27,8 +33,23 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     @Override
-    public EmployeeDto getById(Long id) {
+    public APIResponseDto getById(Long id) {
         Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        return optionalEmployee.map(EmployeeMapper::mapToEmployeeDto).orElse(null);
+        if (optionalEmployee.isPresent()) {
+            ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
+                    "http://localhost:8080/api/v1/departments/" + optionalEmployee.get().getDepartmentCode(),
+                    DepartmentDto.class
+            );
+
+            EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(optionalEmployee.get());
+            DepartmentDto departmentDto = responseEntity.getBody();
+
+            return APIResponseDto.builder()
+                    .employeeDto(employeeDto)
+                    .departmentDto(departmentDto)
+                    .build();
+        }
+
+        return null;
     }
 }
