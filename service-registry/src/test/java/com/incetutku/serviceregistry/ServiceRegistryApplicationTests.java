e672cc529f71c1c494f5078717d00cfbe251b597
Tutku Ince <tutku.ince@outlook.com>
2024-09-30 21:03:51 +0300

    service-registry project starter

---
+++ service-registry/src/test/java/com/incetutku/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incetutku.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
