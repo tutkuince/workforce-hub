8ee431017880ecfd32099449f8b2fa236044a1b0
Tutku Ince <tutku.ince@outlook.com>
2024-10-04 23:48:14 +0300

    configured and implemented for circuit breaker pattern

--- employee-service/src/main/java/com/incetutku/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/incetutku/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,6 +8,7 @@ import com.incetutku.employeeservice.mapper.EmployeeMapper;
 import com.incetutku.employeeservice.repository.EmployeeRepository;
 import com.incetutku.employeeservice.service.APIClient;
 import com.incetutku.employeeservice.service.EmployeeService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -31,6 +32,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
 
+    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
     @Override
     public APIResponseDto getById(Long id) {
         Optional<Employee> optionalEmployee = employeeRepository.findById(id);
@@ -44,7 +46,25 @@ public class EmployeeServiceImpl implements EmployeeService {
                     .departmentDto(departmentDto)
                     .build();
         }
+        return null;
+    }
 
+    public APIResponseDto getDefaultDepartment(Long id, Exception exception) {
+        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
+        if (optionalEmployee.isPresent()) {
+            EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(optionalEmployee.get());
+
+            DepartmentDto departmentDto = DepartmentDto.builder()
+                    .name("R&D Department")
+                    .code("RD001")
+                    .description("Research and Development Department")
+                    .build();
+
+            return APIResponseDto.builder()
+                    .employeeDto(employeeDto)
+                    .departmentDto(departmentDto)
+                    .build();
+        }
         return null;
     }
 }
