5296ce7fdcb25e680fcf9d4f6d90fdd6d20d0853
Tutku Ince <tutku.ince@outlook.com>
2024-10-05 19:04:01 +0300

    organization-service project initializer

---
+++ organization-service/src/main/java/com/incetutku/organizationservice/OrganizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.organizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizationServiceApplication.class, args);
+	}
+
+}
