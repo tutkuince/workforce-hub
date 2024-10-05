9840b8d7c56214f5addae78964bbe8ab93bc407f
Tutku Ince <tutku.ince@outlook.com>
2024-10-05 19:18:12 +0300

    Create Organization JPA Entity and JPA Repository

---
+++ organization-service/src/main/java/com/incetutku/organizationservice/entity/Organization.java
@@ -0,0 +1,27 @@
+package com.incetutku.organizationservice.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "organizations")
+public class Organization {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false, unique = true)
+    private String code;
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+}
---
+++ organization-service/src/main/java/com/incetutku/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package com.incetutku.organizationservice.repository;
+
+import com.incetutku.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
