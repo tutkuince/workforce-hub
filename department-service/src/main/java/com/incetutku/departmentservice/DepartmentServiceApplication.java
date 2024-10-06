9ae51589a7e363def405c938daa583cccf8e5428
Tutku Ince <tutku.ince@outlook.com>
2024-10-06 17:37:14 +0300

    Define General API Information using Annotations

--- department-service/src/main/java/com/incetutku/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/incetutku/departmentservice/DepartmentServiceApplication.java
@@ -1,8 +1,33 @@
 package com.incetutku.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Department Service REST APIs",
+                description = "Department Service REST APIs Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Tutku Ince",
+                        email = "ti@mail.com",
+                        url = "https://github.com/tutkuince"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://github.com/tutkuince"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Department-Service Doc",
+                url = "https://github.com/tutkuince"
+        )
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
